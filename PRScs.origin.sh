65c1f3430934521c911d0f104f3711df0bbbfa3f
Yunfeng Ruan <ryf.alexandria@gmail.com>
2020-08-17 17:17:49 -0400

    Update PRScs.origin.sh

--- PRScs.origin.sh
+++ PRScs.origin.sh
@@ -11,28 +11,19 @@ chr=$SGE_TASK_ID
 source /broad/software/scripts/useuse
 reuse Anaconda3
 
-#Need $Base, $trait, $phi, $n_trn; these parameters are added in "qsub -v ..."
-
-if [ $Base == "EUR" ];
-then pop=eur
-elif [ $Base == "EAS" ];
-then pop=eas
-fi
-trait=SCZ
+#Need $Base, $target, $phi, $n_trn, $pop, $OUT; these parameters are added in "qsub -v ..."
 
 dir=/stanley/huang_lab/home/Y.Ruan/SCZ.PRScsx/
 PRScs=/stanley/huang_lab/home/Y.Ruan/Software/PRScs/PRScs.py
 
 ref_dir=/stanley/huang_lab/home/Y.Ruan/REF/ldblk_1kg_${pop}
-bim_prefix=/stanley/huang_lab//home/Y.Ruan/SCZ.PRScsx/Target/${target}
-
 
 python $PRScs \
  --ref_dir=${ref_dir} \
- --bim_prefix=${bim_prefix} \
- --sst_file=${dir}/Base/PRScs-${Base}.${cohort}-${trait}.txt \
+ --bim_prefix=$dir/Target/${target} \
+ --sst_file=${dir}/Base/$Base \
  --n_gwas=${n_trn} \
  --chrom=${chr} \
  --phi=${phi} \
- --out_dir=${dir}/PRScs-result/${Base}.${cohort}_${target}-${trait}
+ --out_dir=${dir}/PRScs-result/$OUT
 
